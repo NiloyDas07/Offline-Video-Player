a6107b929522b663fb430f40eda6f8463412a138
NSD <niloysd7@gmail.com>
2025-05-25 20:22:19 +0530

    added minified build files

---
+++ build/sw.js
@@ -0,0 +1,1 @@
+const CACHE_NAME="video-player-cache-v1";const ASSETS=["./","./index.html","./index.css","./index.js",];self.addEventListener("install",(evt)=>{evt.waitUntil(caches.open(CACHE_NAME).then((cache)=>cache.addAll(ASSETS)))});self.addEventListener("fetch",(evt)=>{evt.respondWith(caches.match(evt.request).then((response)=>response||fetch(evt.request)))})
